20776f8b1f5391304c267613bc5472cc6f497f25
1-Holopsicon-1 <mfnbrx@gmail.com>
2025-02-03 21:29:42 +0300

    change loading env logic

--- gw-currency-wallet/internal/app/cache/cache.go
+++ gw-currency-wallet/internal/app/cache/cache.go
@@ -2,17 +2,11 @@ package cache
 
 import (
 	redis_store "github.com/eko/gocache/store/redis/v4"
-	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
-	"log"
 	"os"
 )
 
 func getRedisAddress() string {
-	e := godotenv.Load(".env")
-	if e != nil {
-		log.Println("No env")
-	}
 	host := os.Getenv("redis_host")
 	port := os.Getenv("redis_port")
 	return host + ":" + port
